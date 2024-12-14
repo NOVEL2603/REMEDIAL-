a4ee397b45eb9c79f58171220db76f25d59f8303
NOVEL2603 <noveladitamahendra@gmail.com>
2024-12-14 11:58:24 +0700

    admin

--- index.php
+++ index.php
@@ -1,47 +1,54 @@
+<?php
+session_start();
+if ($_SESSION['status'] != 'login' ) {
+  echo "<script>
+  alert('Anda belum login!');
+  location.href='../index.php';
+  </script>";
+}
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Galeri Foto</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
 </head>
 <body>
+<body class="p-3 m-0 border-0 bd-example m-0 border-0">
+
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
     <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
-      <div class="navbar-nav me-auto">
-
-      </div>
-      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
-      <a href="login.php" class="btn btn-outline-success m-1">Masuk</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <a href="album.php" class="nav-link">Album</a>
+        <a href="foto.php" class="nav-link">Foto</a>
+        </div>
+       
+        <a href="../config/aksi_logout.php" class="btn btn-outline-danger m-1">Keluar</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
 
-<div class="container mt-3">
-  <div class="row">
-    <div class="col-md-3">
-      <div class="card">
-        <img src=""class="card-img-top" title="" style="height: 12rem;">
-          <div class="card-footer text-center">
-        <a herf="">56jt triliun suka</a>
-        <a herf="">3 komentar</a>
-      </div>
-     </div>
-    </div>
-   </div>
-  </div>
 
-  <footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
+<footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
     <p>&copy; UKK RPL 2024 | Nama  Peserta</p>
   </footer>
 
     
-<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
 </body>
 </html>
